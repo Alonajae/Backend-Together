bbc9dc90f72c1cd50a66a25a698ae8a620bcb2bc
Thomas <thomas.pierreleandre972@gmail.com>
2023-05-12 17:44:47 +0200

    t

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,5 @@
 var express = require('express');
 var router = express.Router();
-
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const User = require('../models/user');
 
 module.exports = router;
