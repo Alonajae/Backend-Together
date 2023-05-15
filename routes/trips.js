0ed5e15888ba7fa99db76fda4218b909a48e7e2d
Thomas <thomas.pierreleandre972@gmail.com>
2023-05-15 14:16:13 +0200

    t

--- routes/trips.js
+++ routes/trips.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const Trip = require('../models/trips');
+const User = require('../models/users');
+
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,24 @@
 var express = require('express');
 var router = express.Router();
 const User = require('../models/users');
+const Trip = require('../models/trips');
+
+// Get all the trips of a user
+
+router.post('/trips/:token', function (req, res, next) {
+    const token = req.params.token;
+    User.findOne({ token: token })
+        .then((data) => {
+            if (data) {
+                Trip.find({ user: data._id })
+                    .populate('user')
+                    .then((trips) => {
+                        res.json({ result: true, trips: trips });
+                    })
+            }
+        })
+})
+
+
 
 module.exports = router;
