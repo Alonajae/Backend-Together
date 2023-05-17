a78ca362f526ca3b08a482c0b0e75d8c57b8a55c
camillepic <camillejpicard@gmail.com>
2023-05-17 17:08:13 +0200

    modif routes alerts

--- routes/alerts.js
+++ routes/alerts.js
@@ -5,39 +5,37 @@ const Alert = require('../models/alerts');
 // Get all the alerts
 
 router.get('/', function (req, res, next) {
-    SafePlace.find()
+    Alert.find()
+        .populate('user') // Populate the user field of the alert with the user data
         .then((data) => {
-            res.json({ result: true, safeplaces: data });
+            res.json({ result: true, alerts: data });
         })
         .catch((error) => {
-            res.json({ result: false, error: error.message });
+            res.json({ result: false, error: error.message });  
         });
 });
 
-// Add a new alert
 
+// Add a new alert
 router.post('/add', function (req, res, next) {
-    SafePlace.findOne({ address: req.body.address }).then((data) => {
-        if (data) {
-            res.json({ result: false, error: 'This safeplace already exists' });
-        } else {
-    const newSafePlace = new SafePlace({
-        address: req.body.address,
+    const newAlert = new Alert({
+        user: ObjectId(req.body.userId),
         coordinate: {
             latitude: req.body.latitude,
             longitude: req.body.longitude,
         },
-        hours: req.body.hours,
-        phone: req.body.phone,
+        date: req.body.date,
+        type: req.body.type,
+        description: req.body.description,
     });
-    newSafePlace.save()
+    newAlert.save()
         .then((data) => {
-            res.json({ result: true, safeplace: data });
+            res.json({ result: true, alert: data });
         })
         .catch((error) => {
             res.json({ result: false, error: error.message });
-        });     
-}})})
+        });
+})
 
 
 
