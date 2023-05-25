5ddb1e380d8b903c1557dd7278e68953e5a6e7d2
Thomas <thomas.pierreleandre972@gmail.com>
2023-05-25 09:59:44 +0200

    t

--- routes/trips.js
+++ routes/trips.js
@@ -142,7 +142,7 @@ router.post('/sharedtrip', function (req, res, next) {
     const origin = `${from.latitude},${from.longitude}`;
     const destination = `${to.latitude},${to.longitude}`;
 
-    const {waypointsString = waypoints.map(waypoint => `${waypoint.latitude},${waypoint.longitude}`).join('|');
+    const waypointsString = waypoints.map(waypoint => `${waypoint.latitude},${waypoint.longitude}`).join('|');
 
     const directionsUrl = `https://maps.googleapis.com/maps/api/directions/json?origin=${origin}&destination=${destination}&waypoints=${waypointsString}&mode=${mode}$&key=${process.env.GOOGLE_API_KEY}`;
 
